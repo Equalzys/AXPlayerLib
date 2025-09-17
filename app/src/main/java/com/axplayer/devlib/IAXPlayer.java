674c3de24dbfc51dc6e8fc07c068b6fff1ff673b
ZhangYiSheng <zhangyisheng@inshot.com>
2025-09-17 09:50:35 +0800

    修改注释

--- app/src/main/java/com/axplayer/devlib/IAXPlayer.java
+++ app/src/main/java/com/axplayer/devlib/IAXPlayer.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public interface IAXPlayer {
 
-    //后续再支持传入Android Uri(FFmpeg官方已支持)
+    //后续再支持传入Android Uri(FFmpeg7.0及以上官方已支持传入Content Uri，主要函数av_jni_set_java_vm(vm, nullptr)和av_jni_set_android_app_ctx(global_ctx, nullptr))
     void setDataSource(Context context, Uri uri, Map<String, String> headers);
     void setDataSource(String path);
     void prepare();
@@ -27,16 +27,22 @@ public interface IAXPlayer {
     long getDuration();
 
     void setVolume(float leftVolume, float rightVolume);
-
+    //计算宽高比使用：videoSarNum/videoSarDen
     int getVideoSarNum();
+    //计算宽高比使用：videoSarNum/videoSarDen
     int getVideoSarDen();
 
     int getAudioSessionId();
 
+    //准备完毕,回调接口仅用于java层
     void setOnPreparedListener(OnPreparedListener listener);
+    //播放结束,回调接口仅用于java层
     void setOnCompletionListener(OnCompletionListener listener);
+    //缓冲进度,回调接口仅用于java层
     void setOnBufferingUpdateListener(OnBufferingUpdateListener listener);
+    //视频宽高变化大小,回调接口仅用于java层
     void setOnVideoSizeChangedListener(OnVideoSizeChangedListener listener);
+    //播放出错,回调接口仅用于java层
     void setOnErrorListener(OnErrorListener listener);
 
     interface OnPreparedListener {
